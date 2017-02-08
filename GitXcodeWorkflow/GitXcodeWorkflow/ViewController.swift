7ad7a70d0485af67a6036d77ee7c21f801a0f2a4
Michael Tompkins <tompkini@mwc-035108.dhcp.missouri.edu>
2017-02-08 13:20:56 -0600

    Added functionality to switch

--- GitXcodeWorkflow/GitXcodeWorkflow/ViewController.swift
+++ GitXcodeWorkflow/GitXcodeWorkflow/ViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switcherState: UILabel!
+    @IBOutlet weak var theSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sayHello()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func switchingSwitcher(_ sender: Any) {
+        sayHello()
+    }
     
     func sayHello(){
-        print("Goodbye World")
+        if theSwitch.isOn{
+            switcherState.text = "On"
+        }else{
+            switcherState.text = "Off"
+        }
     }
     
     func sayGoodbye(){
